6d2870ea2373caf133737f685ea3253007684680
“nvtamnt99” <nvtamnt99@gmail.com>
2021-09-26 21:36:28 +0700

    Controller and Router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3000
+const port = 3000;
+
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // HTTP logger
 //app.use(morgan('combined'));
@@ -14,21 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('hbs', handlebars({
     extname: ".hbs"
 }));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    console.log(req.query.q);
-    res.render('search');
-});
+//Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
