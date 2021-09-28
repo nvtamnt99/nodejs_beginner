764acc69e524f3d50eed5d2512c0a23136c26903
“nvtamnt99” <nvtamnt99@gmail.com>
2021-09-28 18:05:18 +0700

    code model connect db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,28 @@
+const Course = require('../models/Course')
+const { multipleMongooseToObject } = require('../../util/mongoose');
+
 class SiteController {
 
     //[GET] /
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+
+        // Course.find({}, function (err, courses) {
+        //     if (!err) {
+        //         res.json(courses);
+        //     } else {
+        //         res.status(400).json({err: 'ERROR!!'});
+        //     }
+        // });
+
+        Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses)
+                });
+            })
+            .catch(next);
+
+        // res.render('home');
     }
 
     //[GET] /
